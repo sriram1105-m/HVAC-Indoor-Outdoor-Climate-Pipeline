31a429ac6de04cdb7339c061a83d09ea369f79eb
Sriram Murali <75327547+sriram1105-m@users.noreply.github.com>
2025-08-27 15:14:10 +0530

    Update create_gold_table.sql

--- sql/create_gold_table.sql
+++ sql/create_gold_table.sql
@@ -1,9 +1,30 @@
-CREATE TABLE dbo.IndoorOutdoorClimate (
+-- Gold: Indoor Aggregated Table
+CREATE TABLE [gold].[iot_indoor_aggregated] (
+    event_time DATETIME,
+    indoor_temperature FLOAT,
+    indoor_humidity FLOAT,
+    device_id NVARCHAR(50),
+    location NVARCHAR(100)
+);
+
+-- Gold: Outdoor Aggregated Table
+CREATE TABLE [gold].[iot_outdoor_aggregated] (
+    event_time DATETIME,
+    outdoor_temperature FLOAT,
+    outdoor_humidity FLOAT,
+    weather_code INT,
+    wind_speed FLOAT,
+    location NVARCHAR(100)
+);
+
+-- Gold: Indoor vs Outdoor Combined Table
+CREATE TABLE [gold].[iot_indoor_outdoor_aggregated] (
     event_time DATETIME,
     indoor_temperature FLOAT,
     indoor_humidity FLOAT,
     outdoor_temperature FLOAT,
     outdoor_humidity FLOAT,
     temperature_delta FLOAT,
-    humidity_delta FLOAT
+    humidity_delta FLOAT,
+    location NVARCHAR(100)
 );
