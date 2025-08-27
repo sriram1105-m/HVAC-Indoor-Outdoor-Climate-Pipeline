80e911dbf8a5d44e94dd2e3c20ef9f0df2417cf0
Sriram Murali <75327547+sriram1105-m@users.noreply.github.com>
2025-08-27 15:08:10 +0530

    Create create_gold_table.sql

---
+++ sql/create_gold_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dbo.IndoorOutdoorClimate (
+    event_time DATETIME,
+    indoor_temperature FLOAT,
+    indoor_humidity FLOAT,
+    outdoor_temperature FLOAT,
+    outdoor_humidity FLOAT,
+    temperature_delta FLOAT,
+    humidity_delta FLOAT
+);
